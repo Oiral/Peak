9f66c760759491a15d3a16e299a0955dce18d930
Hamish <Hamish@pageland.co.nz>
2019-09-07 07:14:18 +1200

    Moved some items and added in procedural level

---
+++ Assets/Scripts/Level Generation/RandomGenStorageSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Section Info", order = 1)]
+public class RandomGenStorageSO : ScriptableObject
+{
+    public List<GameObject> middleSection;
+    public List<GameObject> baseSection;
+    public List<GameObject> floor;
+}
---
+++ Assets/Scripts/Level Generation/Section.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Section : MonoBehaviour
+{
+    public bool drawOnSelected;
+    private void OnDrawGizmos()
+    {
+        if (drawOnSelected == false)
+        {
+            DrawTheGizmos();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (drawOnSelected)
+        {
+            DrawTheGizmos();
+        }
+    }
+
+    void DrawTheGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    [Header("Info")]
+    public Vector3 size = Vector3.one;
+}
