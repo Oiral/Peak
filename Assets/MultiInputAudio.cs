7f578004351301072fbf7048941377e1af816e42
Hamish <Hamish@pageland.co.nz>
2019-10-06 06:45:12 +1300

    new 'footstep' player audio

---
+++ Assets/MultiInputAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiInputAudio : MonoBehaviour
+{
+
+    public MultiInputAudioMaster audioMaster;
+
+    public float minHit;
+    public float maxHit;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > minHit)
+        {
+            audioMaster.PlaySound(Mathf.Clamp(collision.relativeVelocity.magnitude / maxHit, 0, 1));
+        }
+    }
+}
---
+++ Assets/MultiInputAudioMaster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiInputAudioMaster : MonoBehaviour
+{
+    public float delayTime;
+    float delayTimer;
+
+    AudioSource source;
+
+    float volumeAverage;
+    float averageCount;
+
+    private void Update()
+    {
+        if (delayTimer > 0)
+        {
+            delayTimer -= Time.deltaTime;
+            source.volume = volumeAverage / averageCount;
+
+            if (delayTimer > 0)
+            {
+                averageCount = 0;
+                volumeAverage = 0;
+            }
+        }
+    }
+
+    public void PlaySound(float volume)
+    {
+        volumeAverage += volume;
+        averageCount += 1;
+        if (delayTimer <= 0)
+        {
+            source.Play();
+            delayTimer = delayTime;
+        }
+    }
+}
