d840471d160a57a4636616eb9d965e7de9c9ab3c
Jazz <jazzcousins@gmail.com>
2019-11-12 02:40:52 +0000

    New 3 finger hands and more level model stuff

--- Assets/Scripts/PhysicsHand.cs
+++ Assets/Scripts/PhysicsHand.cs
@@ -13,9 +13,9 @@ public class PhysicsHand : MonoBehaviour
     public GameObject Thumb;
     public GameObject FI;
     public GameObject FM;
-    public GameObject FR;
-    public GameObject FP;
-        
+    //public GameObject FR;
+    //public GameObject FP;
+
     public GameObject nearHand;
     public GameObject grabbed;
     private void OnTriggerEnter(Collider other)
@@ -35,7 +35,7 @@ public class PhysicsHand : MonoBehaviour
     //public SteamVR_Action Squeeze; //Squeeze is the trigger axes, select from inspecter
 
     public SteamVR_Action_Boolean grabPinch; //Grab Pinch is the trigger, select from inspecter
-  
+
     public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;//which controller
                                                                          // Use this for initialization
 
@@ -45,13 +45,13 @@ public class PhysicsHand : MonoBehaviour
         Thumb.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
         FI.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
         FM.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
-        FR.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
-        FP.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
+        //FR.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
+        //FP.GetComponent<SpringJoint>().spring = SteamVR_Input.GetFloat("Squeeze", inputSource) * 5500 - 50;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+
         if (collision.relativeVelocity.magnitude > minHitAmount)
         {
             ParticleSystem particle = Instantiate(hitCloudPrefab, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal), null).GetComponent<ParticleSystem>();
@@ -60,7 +60,7 @@ public class PhysicsHand : MonoBehaviour
             module.startSpeedMultiplier = module.startSpeedMultiplier * Mathf.Clamp(collision.relativeVelocity.magnitude / bigHitAmount, 0, 2);
         }
     }
-    
+
 
     void OnEnable()
     {
