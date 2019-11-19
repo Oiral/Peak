9155d719579d1b33b6c67f58f45c848f51c525d6
Hamish <Hamish@pageland.co.nz>
2019-11-20 07:23:25 +1300

    Better Jetpack script that uses particles

---
+++ Assets/Scripts/Power ups/PowerBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBase : MonoBehaviour
+{
+    public float maxFuel = 100;
+    public float currentFuel;
+
+    public void Charge(float amount)
+    {
+        currentFuel += amount;
+        if (currentFuel > maxFuel)
+        {
+            currentFuel = maxFuel;
+        }
+    }
+
+    public bool DrainOverTime(float multiplier)
+    {
+        currentFuel -= Time.deltaTime * multiplier;
+        if (currentFuel <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public bool Drain(float amount)
+    {
+        //Only drain if we can actually use the power up otherwise don't drain
+        if (currentFuel - amount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            currentFuel -= amount;
+            return true;
+        }
+    }
+}
--- Assets/Scripts/Power ups/PowerRocket.cs
+++ Assets/Scripts/Power ups/PowerRocket.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PhysicsHand))]
+[RequireComponent(typeof(PowerBase))]
 public class PowerRocket : MonoBehaviour
 {
     #region Power Stuff
     PhysicsHand hand;
+
+    PowerBase basePower;
 
     public Vector3 direction;
 
@@ -15,22 +18,54 @@ public class PowerRocket : MonoBehaviour
     private void Awake()
     {
         hand = GetComponent<PhysicsHand>();
+        basePower = GetComponent<PowerBase>();
     }
 
     private void OnEnable()
     {
         hand.gripHold.AddListener(jetPack);
+        hand.gripUp.AddListener(jetPackOff);
+
+        spawnedJetPackObject = Instantiate(jetPackPrefab, transform.position, transform.rotation, transform);
+        particles.AddRange(spawnedJetPackObject.GetComponentsInChildren<ParticleSystem>());
+        jetPackOff();
     }
 
     private void OnDisable()
     {
         hand.gripHold.RemoveListener(jetPack);
+        hand.gripUp.RemoveListener(jetPackOff);
+
+        Destroy(spawnedJetPackObject);
     }
     #endregion
 
+    public List<ParticleSystem> particles;
+
+    public GameObject jetPackPrefab;
+    GameObject spawnedJetPackObject;
+
     void jetPack()
     {
-        Debug.Log("Testing Rocket");
-        GetComponent<Rigidbody>().AddForce((transform.forward + direction).normalized * force, ForceMode.VelocityChange);
+        if (basePower.DrainOverTime(10))
+        {
+            Debug.Log("Testing Rocket");
+            GetComponent<Rigidbody>().AddForce((transform.forward + direction).normalized * force, ForceMode.VelocityChange);
+            toggleParticles(true);
+        }
     }
+
+    void jetPackOff()
+    {
+        toggleParticles(false);
+    }
+
+    void toggleParticles(bool toggle)
+    {
+        for (int i = 0; i < particles.Count; i++)
+        {
+            particles[i].enableEmission = toggle;
+        }
+    }
+    
 }
