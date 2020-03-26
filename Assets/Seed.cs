fcb4487bc72a8eef0c5a1e7ecfa39a10d42232a1
Hamish <Hamish@pageland.co.nz>
2020-03-26 21:43:34 +1300

    Tidied up the generation

--- Assets/Seed.cs
+++ Assets/Seed.cs
@@ -1,11 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Seed : MonoBehaviour
 {
     public RandomGenStorageSO storage;
-    public int towerSeed;
+    
+    public int towerSeed {
+        get {
+            return internalSeed;
+        }
+        set {
+            internalSeed = value;
+            UpdateSeedInfo();
+        }
+    }
 
+    [SerializeField]
+    int internalSeed;
+
+    public void UpdateSeedInfo()
+    {
+        //Maybe change the look depending on the storage type?
+        //Change the seed text
+        GetComponentInChildren<Text>().text = internalSeed.ToString();
+    }
 
 }
