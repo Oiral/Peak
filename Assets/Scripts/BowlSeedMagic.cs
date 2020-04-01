bf205f9caa41620944414424d60d9d982c24e79e
Jazz <jazzcousins@gmail.com>
2020-04-01 12:53:10 +0100

    model and scripting seed fountain stuff

---
+++ Assets/Scripts/BowlSeedMagic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlSeedMagic : MonoBehaviour
+{
+
+    public float GrabForce = 20000f;
+    public Transform GrabPoint;
+
+    private Rigidbody Seed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Seed != null)
+        {
+            Seed.drag = 5;
+            Seed.useGravity = false;
+            Seed.AddForce(GrabPoint.position - Seed.transform.position * GrabForce);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<Rigidbody>().gameObject.tag == "Pickupable")
+            if(Seed != null)
+            {
+                Seed.drag = 0;
+                Seed.useGravity = true;
+            }
+            Seed = other.GetComponentInParent<Rigidbody>();
+    }
+}
+
