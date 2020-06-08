663122a3ca122dfd814a032e55c8f7657fccd768
Hamish <Hamish@pageland.co.nz>
2020-06-08 21:26:56 +1200

    Updated seeds for different towers

--- Assets/Seed.cs
+++ Assets/Seed.cs
@@ -47,29 +47,57 @@ public class Seed : MonoBehaviour
             if (storage.seedVisualPrefab != null)
             {
                 //If we actually have visuals to change to
-                //Destroy the current seed visual
+                if (currentSeedVisual != null)
+                {
 #if UNITY_EDITOR
-                Debug.LogError("Trying to delete seed during edit mode. Will work in play mode - Just shrunk to 0,0,0");
-                currentSeedVisual.transform.localScale = Vector3.zero;
+                    Debug.LogError("Trying to delete seed during edit mode. Will work in play mode - Just shrunk to 0,0,0");
+                    //If there is a seed visual to change
+                    currentSeedVisual.transform.localScale = Vector3.zero;
 #else
-                Destroy(currentSeedVisual);
+                    //Destroy the current seed visual
+                    Destroy(currentSeedVisual);
 
 #endif
-
+                }
                 //Spawn in the new visual
                 currentSeedVisual = Instantiate(storage.seedVisualPrefab, transform.position, transform.rotation, transform);
             }
         }
 
-        if (blankSeed)
+        TextMeshPro displayText = null;
+
+        if (currentSeedVisual == null)
         {
-            //Change the seed text
-            GetComponentInChildren<TextMeshPro>().text = "Blank Seed";
+            displayText = GetComponentInChildren<TextMeshPro>();
         }
         else
         {
-            //Change the seed text
-            GetComponentInChildren<TextMeshPro>().text = internalSeed.ToString();
+            displayText = currentSeedVisual.GetComponentInChildren<TextMeshPro>();
+
+            //If we can't find the display text in the seed visual, lets look for it in the seed
+            if (displayText == null)
+            {
+                displayText = GetComponentInChildren<TextMeshPro>();
+            }
+        }
+
+
+        if (displayText != null)
+        {
+            if (blankSeed)
+            {
+                //Change the seed text
+                displayText.text = "Blank Seed";
+            }
+            else
+            {
+                //Change the seed text
+                displayText.text = internalSeed.ToString();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Cannot find text mesh pro for seed display", gameObject);
         }
         
     }
