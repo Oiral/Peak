76a28f6a5730135e38ffed255bd0a9f4d5313761
Hamish <Hamish@pageland.co.nz>
2020-06-16 07:13:39 +1200

    Added in floating player vfx and basic physics to hold them up

---
+++ Assets/FloatPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatPlayer : MonoBehaviour
+{
+    public bool floatIsActive;
+
+    public Rigidbody bitToFloat;
+
+    public Transform standingPos;
+
+    public float upForce;
+
+    Vector3 posDif;
+
+    private void Update()
+    {
+        if (floatIsActive)
+        {
+            posDif = standingPos.position - bitToFloat.transform.position;
+
+
+            if (posDif.y > 0)
+            {
+                bitToFloat.AddForce(0, upForce, 0, ForceMode.Acceleration);
+            }
+        }
+    }
+
+}
