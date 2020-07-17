b7f788511ac269f94dd851ddbb108c1b1b87ea3f
Hamish <Hamish@pageland.co.nz>
2020-07-17 22:24:00 +1200

    Tweaks to starting section and seeds

---
+++ Assets/Scripts/GizmoMeshRender.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoMeshRender : MonoBehaviour
+{
+    public Color GizmoColor = Color.red;
+
+    private void OnDrawGizmosSelected()
+    {
+        Mesh displayMesh = GetComponent<MeshFilter>().sharedMesh;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.color = GizmoColor;
+        Gizmos.DrawMesh(displayMesh, Vector3.zero, Quaternion.identity);
+    }
+}
